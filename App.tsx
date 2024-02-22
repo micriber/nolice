a7102faf34e8d36cda81a1d25943a65f623031d2
Coquelet Christophe <coquelet.c@gmail.com>
2024-02-22 17:58:48 +0100

    Feat/add game menu (#1)
    * chore: Add svg loader dependencies
    
    * chore: Add logo in jpeg and svg file
    
    * feat(menu): Add menu screen with logo and play button

--- App.tsx
+++ App.tsx
@@ -1,21 +1,78 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Button, TouchableOpacity } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+import { Logo } from './src/svg/logo'
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hey Micriber! I'm Nolice</Text>
-      <Text>A very powerful React Native / Expo app</Text>
-      <StatusBar style="auto" />
+    <>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Menu"
+          component={MenuScreen}
+          options={{title: 'Menu'}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+	<StatusBar style="auto" />
+	</>
+  );
+};
+
+function MenuScreen() {
+  return (
+    <View
+      style={[
+        styles.container,
+        {
+          // Try setting `flexDirection` to `"row"`.
+          flexDirection: 'column',
+        },
+      ]}>
+      <View style={styles.header}>
+        <Logo />
+      </View>
+      <View style={styles.body}>
+        <TouchableOpacity
+          style={styles.playButton}
+          onPress={() => {}}
+        >
+          <Text style={styles.playButtonText}>JOUER</Text>
+        </TouchableOpacity>
+      </View>
+      {/* <View style={{flex: 3, backgroundColor: 'green'}} /> */}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  header: {
+    flex: 1,
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    padding: 20,
+  },
+  body: {
+    flex: 1,
+    // height: "50%",
     justifyContent: 'center',
   },
+  playButton: {
+    alignItems: 'center',
+    backgroundColor: '#3d8c40',
+    height: 50,
+    // width: 150,
+    justifyContent: 'center',
+  },
+  playButtonText: {
+    fontSize: 20,
+    // fontWeight: "bold",
+    color: "#081108"
+  }
 });
