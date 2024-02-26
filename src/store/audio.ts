44ee9edf239a3575667c2f6b9cd9d57ea053e122
CARON Bernard <caronbernard1@gmail.com>
2024-02-26 16:02:35 +0100

    feat: add other animal (#10)

--- src/store/audio.ts
+++ src/store/audio.ts
@@ -10,6 +10,13 @@ interface AudioStoreState {
 const DOMMAGE_SOUND_PATH = '../../assets/audio/dommage.mp3'
 const BRAVO_SOUND_PATH = '../../assets/audio/bravo.mp3'
 const COUNT_DUCK_SOUND_PATH = '../../assets/audio/duck.mp3'
+const COUNT_CAT_SOUND_PATH = '../../assets/audio/cat.mp3'
+const COUNT_DOG_SOUND_PATH = '../../assets/audio/dog.mp3'
+const COUNT_PIG_SOUND_PATH = '../../assets/audio/pig.mp3'
+const COUNT_COW_SOUND_PATH = '../../assets/audio/cow.mp3'
+const COUNT_RABBIT_SOUND_PATH = '../../assets/audio/rabbit.mp3'
+const COUNT_BIRD_SOUND_PATH = '../../assets/audio/bird.mp3'
+const COUNT_SHEEP_SOUND_PATH = '../../assets/audio/sheep.mp3'
 const CONGRATULATION_SOUND_PATH = '../../assets/audio/congratulation.mp3'
 const RETRY_SOUND_PATH = '../../assets/audio/retry.mp3'
 
@@ -19,7 +26,14 @@ export const SOUNDS = {
   BRAVO: require(BRAVO_SOUND_PATH),
   CONGRATULATION: require(CONGRATULATION_SOUND_PATH),
   COUNT: {
-    DUCK: require(COUNT_DUCK_SOUND_PATH)
+    DUCK: require(COUNT_DUCK_SOUND_PATH),
+    CAT: require(COUNT_CAT_SOUND_PATH),
+    DOG: require(COUNT_DOG_SOUND_PATH),
+    PIG: require(COUNT_PIG_SOUND_PATH),
+    COW: require(COUNT_COW_SOUND_PATH),
+    RABBIT: require(COUNT_RABBIT_SOUND_PATH),
+    BIRD: require(COUNT_BIRD_SOUND_PATH),
+    SHEEP: require(COUNT_SHEEP_SOUND_PATH),
   }
 }
 
