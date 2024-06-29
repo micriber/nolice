4dac023884cc4fab092153f98314615a71238ab5
pawndev <coquelet.c@gmail.com>
2024-06-29 21:41:43 +0200

    lint: init linter

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    root: true,
+    extends: [
+        'universe/native',
+        'universe/shared/typescript-analysis',
+    ],
+    overrides: [
+        {
+            files: [
+                '*.ts',
+                '*.tsx',
+                '*.d.ts'
+            ],
+            parserOptions: {
+                project: './tsconfig.json'
+            },
+        },
+    ],
+};
